d8c2ec0d6eb9d1fded6890b086997cb16ec075b4
Muhammad Talha Imran <92talhaimran@gmail.com>
2025-08-30 00:24:23 +0200

    Fix build and JSX syntax errors

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+'use client';
+
 import { useState } from 'react';
 import { Mail, CheckCircle, AlertCircle, ArrowUpRight } from 'lucide-react';
 
--- components/PortfolioGrid.tsx
+++ components/PortfolioGrid.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import { ArrowUpRight, BookOpen, PenTool, Mic, Calendar, Star } from 'lucide-react';
